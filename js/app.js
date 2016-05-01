084e7c129803e954e1c84afdbe1b8c44456cfd2f
Oliver Klein <me@oliverkle.in>
2016-05-01 15:40:50 -0700

    New design style with responsive design

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-
+window.onload=function(){
+	var opacity = window.innerWidth / 1600;
+	var collection = document.getElementsByClassName("image");
+	var arr = [].slice.call(collection);
+	arr.forEach(function(element){
+		element.style.opacity = opacity*opacity;
+	})
+};
+window.onresize=function(){
+	var opacity = window.innerWidth / 1600;
+	var collection = document.getElementsByClassName("image");
+	var arr = [].slice.call(collection);
+	arr.forEach(function(element){
+		element.style.opacity = opacity*opacity;
+	})
+};
